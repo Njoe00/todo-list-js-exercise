51adb571cd924255ffb9ee4cf73052df91be9ceb
Njoe00 <nick.james.joe@gmail.com>
2022-11-25 21:27:30 +0000

    created task function with methods and OO programming

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// Arrays to keep track of each task's state
-function newTask(title, desrcription) {
+function newTask(title, description) {
   const task = {
-    title: title,
-    desrcription: desrcription,
+    title,
+    description,
     complete: false,
-  
+
+
     logState: function() {
-      console.log(`${this.title} has${this.complete ? " " : " not"} been completed`);
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
     markCompleted: function() {
@@ -16,8 +16,9 @@ function newTask(title, desrcription) {
   return task;
 }
 
-const task1 = newTask("Clean Cat Litter", "Take all the poo out of the litter box");
-const task2 = newTask("Do Laundry, sad");
+
+const task1 = newTask("Clean Cat Litter", "Take; all the poo out of the litter box");
+const task2 = newTask("Do Laundry", "yuck");
 const tasks = [task1, task2];
 
 task1.logState();
